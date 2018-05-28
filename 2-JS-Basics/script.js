2c5ccdd1ce3879f7aa2ff1b751e3ee88ec962c74
Chris Howe-Jones <chris.howejones@gmail.com>
2018-05-28 15:42:58 +0100

    Completed Section 2 of course.

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -215,7 +215,7 @@ yearsUntilRetirement("Mary", 1948);
 
 ////////////////////////////////////////
 // Lecture: Statements and expressions
-
+/*
 // function statement
 function someFun(par) {
     // code
@@ -234,3 +234,195 @@ var x = 3;
 if (x == 5) {
     // do something
 }
+
+*/
+
+////////////////////////////////////////
+// Lecuture: Arrays
+/*
+var names = ["John", "Jane", "Mark"];
+var years = new Array(1990, 1969, 1948);
+
+// read value for John
+console.log(names[0]);
+names[1] = "Ben";
+console.log(names);
+
+// name, lastName, yearOfBirth, isMarried
+var john = ["John", "Smith", 1990, "designer", false];
+
+john.push("blue");
+john.unshift("Mr");
+var favColour = john.pop();
+john.shift();
+console.log(john);
+console.log(favColour);
+
+if (john.indexOf("teacher") === -1) {
+    console.log("John is not a teacher");
+}
+
+*/
+
+////////////////////////////////////////
+// Lecture: Objects and properties
+/*
+var john = {
+    name: "John",
+    lastName: "Smith",
+    yearOfBirth: 1990,
+    job: "teacher",
+    isMarried: false
+};
+
+console.log(john.lastName);
+console.log(john["lastName"]);
+
+var xyz = "job";
+console.log(john[xyz]);
+
+john.lastName = "Miller";
+john["job"] = "programmer";
+
+console.log(john);
+
+var jane = new Object();
+jane.name = "Jane";
+jane.lastName = "Smith";
+jane["yearOfBirth"] = 1969;
+jane["job"] = "retired";
+jane["isMarried"] = true;
+
+console.log(jane);
+*/
+
+////////////////////////////////////////
+// Lecture: Objects and methods
+
+// v1.0
+/*
+var john = {
+    name: "John",
+    lastName: "Smith",
+    yearOfBirth: 1990,
+    job: "teacher",
+    isMarried: false,
+    family: ["Jane", "Mark", "Bob"],
+    calculateAge: function() {
+        return 2016 - this.yearOfBirth;
+    }
+};
+
+console.log(john.calculateAge());
+
+var age = john.calculateAge();
+john.age = age;
+
+console.log(john);
+*/
+
+// v2.0
+/*
+var john = {
+    name: "John",
+    lastName: "Smith",
+    yearOfBirth: 1990,
+    job: "teacher",
+    isMarried: false,
+    family: ["Jane", "Mark", "Bob"],
+    calculateAge: function() {
+        this.age = 2016 - this.yearOfBirth;
+    }
+};
+
+john.calculateAge();
+
+console.log(john);
+
+var mike = {
+    name: "Mike",
+    lastName: "Smith",
+    yearOfBirth: 1950,
+    job: "teacher",
+    isMarried: false,
+    family: ["Jane", "Mark", "Bob"],
+    calculateAge: function() {
+        this.age = 2016 - this.yearOfBirth;
+    }
+};
+
+mike.calculateAge();
+
+console.log(mike);
+*/
+
+////////////////////////////////////////
+// Lecture: Loops and iteration
+/*
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+
+var names = ["John", "Jane", "Mary", "Mark", "Bob"];
+
+
+// for loops
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
+
+for (var i = names.length - 1; i >= 0; i--) {
+    console.log(names[i]);
+}
+
+// while loops
+var i = 0;
+while (i < names.length) {
+    console.log(names[i]);
+    i++;
+}
+
+for (var i = 1; i <= 10; i++) {
+    if (i === 3) {
+        continue;
+    }
+
+    console.log(i);
+
+    if (i === 5) {
+        break;
+    }
+}
+*/
+
+////////////////////////////////////////
+// Lecture; Coding Challenge 2
+var birthYears = [2001, 1985, 2008, 1992];
+
+function fullAge(yearsOfBirth) {
+    var ages = [];
+    for (var i = 0; i < yearsOfBirth.length; i++) {
+        ages.push(2018 - yearsOfBirth[i]);
+    }
+
+    var fullAges = [];
+
+    for (i = 0; i < ages.length; i++) {
+        if (ages[i] >= 18) {
+            console.log("Person " + (i + 1) +
+                        " is " + ages[i] +
+                        " years old, and is of full age.");
+            fullAges.push(true);
+        } else {
+            console.log("Person " + (i + 1) +
+                        " is " + ages[i] +
+                        " years old, and is NOT of full age.");
+            fullAges.push(false);
+
+        }
+    }
+    return fullAges;
+}
+
+console.log(fullAge(birthYears));
