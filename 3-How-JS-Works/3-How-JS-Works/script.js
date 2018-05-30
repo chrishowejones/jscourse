c21e89a8296debd00c523cedb9fb89c8e608d2a0
Chris Howe-Jones <chris.howejones@gmail.com>
2018-05-30 12:58:26 +0100

    Finished section 3.

--- 3-How-JS-Works/3-How-JS-Works/script.js
+++ 3-How-JS-Works/3-How-JS-Works/script.js
@@ -1,21 +1,37 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
+/*
+calculateAge(1955);
+
+function calculateAge(year) {
+    console.log(2016 - year);
+}
 
 
+// won't work as can't hoist fn expression.
+// retirement(1965);
 
 
+var retirement = function(year) {
+    console.log(65 - (2016 - year));
+}
+
+retirement(1990);
 
 
+//variables
+console.log(age); // age = undefined
+var age = 23;
+console.log(age); // age = 23
 
-
-
-
-
-
-
-
-
-
+function foo() {
+    console.log(age); // age = undefined
+    var age = 65;
+    console.log(age); // age = 65
+}
+foo();
+console.log(age); // age = 23
+*/
 
 ///////////////////////////////////////
 // Lecture: Scoping
@@ -67,11 +83,38 @@ function third() {
 ///////////////////////////////////////
 // Lecture: The this keyword
 
+// console.log(this);
 
+// calculateAge(1990);
 
+// function calculateAge(year) {
+//     console.log(2016 - year);
+//     console.log(this);
+// }
 
+var john = {
+    name: "John",
+    yearOfBirth: 1990,
+    calculateAge: function() {
+        console.log(this); // John object
+        console.log(2016 - this.yearOfBirth);
+        /*
+        function innerFunction() {
+            console.log(this); // Window object
+        }
 
+        innerFunction();
+        */
+    }
+};
 
+ john.calculateAge();
 
+var mike = {
+    name: "Mike",
+    yearOfBirth: 1984
 
+};
 
+mike.calculateAge = john.calculateAge;
+mike.calculateAge();
